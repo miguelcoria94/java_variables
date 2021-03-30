fe80938f29e8fe20286eb8f1ea96295497970ce0
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-29 19:05:52 -0500

    intial setup

---
+++ src/variables/variables.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int value1 = 1 , value2=(2*2) - 1;
+		value2=5;
+		System.out.println(value2 + value1);
+	}
+}
